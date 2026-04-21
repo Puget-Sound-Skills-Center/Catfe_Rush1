94b9670e3d4426c53a87c31124475845d2e9f892
Nayelir295 <5019504@STU.HIGHLINESCHOOLS.ORG>
2026-04-21 13:46:15 -0700

    Tab stuff

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControl : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    //Start is called before the first frame update
+    void Start()
+    {
+        menuCanvas.SetActive(false);
+    }
+
+    //Updarte is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
+    }
+}
